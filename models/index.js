f856ed77b0e3e5096e273c53f3a17a591aaf877c
crimsondrac1 <doug@chillopolis.com>
2022-05-30 16:59:31 -0500

    Added comment EPs

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 // create associations
 User.hasMany(Post, {
@@ -22,10 +23,12 @@ Post.belongsToMany(User, {
   through: Vote,
   as: "voted_posts",
   foreignKey: "post_id",
+  onDelete: "cascade",
 });
 
 Vote.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "cascade",
 });
 
 Vote.belongsTo(Post, {
@@ -40,4 +43,20 @@ Post.hasMany(Vote, {
   foreignKey: "post_id",
 });
 
-module.exports = { User, Post, Vote };
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Vote, Comment };
